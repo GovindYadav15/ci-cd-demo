06807aa36027e12826c10f6ce4ed7ae64dde005a
Goivnd Kumar Yadav <robertgovind@gmail.com>
2026-06-10 21:25:57 +0545

    initial setup

---
+++ app/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "CI/CD Demo App Running" });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+const PORT = 3000;
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
